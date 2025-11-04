9ef97ebd594c324a7431343965b6e77d6ce471b4
ch1ng1z10z <ggckam228@main.ru>
2025-11-04 23:29:46 +0600

    f

--- product/catalog/urls.py
+++ product/catalog/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.CategoryWithCountListView.as_view()),  
+    path('categories/', views.CategoryWithCountListView.as_view()),
     path('categories/<int:pk>/', views.CategoryDetailView.as_view()),
+
     path('products/', views.ProductListView.as_view()),
     path('products/<int:pk>/', views.ProductDetailView.as_view()),
-    path('products/reviews/', views.ProductReviewsListView.as_view()),  
+
+    path('products/reviews/', views.ProductReviewsListView.as_view()),
+
     path('reviews/', views.ReviewListView.as_view()),
     path('reviews/<int:pk>/', views.ReviewDetailView.as_view()),
 ]
--- product/catalog/views.py
+++ product/catalog/views.py
@@ -1,36 +1,43 @@
-from django.db.models import Avg, Count
 from rest_framework import generics
+from django.db.models import Avg, Count
+
 from .models import Category, Product, Review
 from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
 
 
-class CategoryListView(generics.ListAPIView):
+
+class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class CategoryDetailView(generics.RetrieveAPIView):
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 
-class ProductListView(generics.ListAPIView):
+
+class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-class ProductDetailView(generics.RetrieveAPIView):
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 
-class ReviewListView(generics.ListAPIView):
+class ReviewListView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-class ReviewDetailView(generics.RetrieveAPIView):
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
 
 
 class ProductReviewsListView(generics.ListAPIView):
@@ -44,4 +51,4 @@ class CategoryWithCountListView(generics.ListAPIView):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.annotate(products_count=Count('product'))
+        return Category.objects.annotate(products_count=Count('products'))
